be02c5ad8422a29001e1ece1f3871e1bfee416d5
Ammar Alkhooly <ammaralkhooly1@gmail.com>
2022-08-04 23:31:51 +0300

    code refactors

---
+++ utils/colors.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+export const info = chalk.hex('#0096FF'); // bright blue color
+export const green = chalk.hex('#85bb65'); // green color
+export const blue = chalk.hex('#0096FF'); // blue color
+export const warn = chalk.hex('#FFCC00'); // orange color
+export const orange = chalk.hex('#ff8c00'); // dark orange color
--- utils/spinner.js
+++ utils/spinner.js
@@ -1,12 +1,12 @@
-import chalk from 'chalk';
 import ora from 'ora';
-const yellow = chalk.hex('#FFFF00.'); // bright yellow color
+
+import { orange } from './colors';
 
 const spinner = new ora({ text: '' });
 const dots = new ora({ text: '', spinner: 'material' });
 
 export const startSpinner = txt =>
-	`${spinner.start(`${yellow(txt)}`)}${dots.start()}`;
+	`${spinner.start(`${orange(txt)}`)}${dots.start()}`;
 
 export const stopSpinner = () => {
 	spinner.stop();
